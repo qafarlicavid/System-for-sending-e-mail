211e3aaff96ad80d41fe45fdeacf587a84643005
qafarlicavid <javidig@code.edu.az>
2022-11-27 19:14:33 +0400

    EmailService is created

---
+++ System for sending e-mail/System for sending e-mail/EmailService/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace System_for_sending_e_mail.EmailService
+{
+    public interface IEmailSender
+    {
+        void SendEmail(Message message);
+    }
+}
---
+++ System for sending e-mail/System for sending e-mail/EmailService/Message.cs
@@ -0,0 +1,20 @@
+﻿using MimeKit;
+
+namespace System_for_sending_e_mail.EmailService
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public Message(List<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+
+            To.AddRange(to.Select(x => new MailboxAddress(string.Empty.ToString(),x.ToString())));
+            Subject = subject;
+            Content = content;   
+        }
+    }
+}
